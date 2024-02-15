6748586399dac16f3220d83901fb4cdb4421393d
Anshorimuslim <ans4175@gmail.com>
2024-02-15 15:24:14 +0700

    chore: readme

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const BatchDownloadTpsC1 = require('./src/runners/BatchDownloadTpsC1')
 const cron = require('node-cron');
 
-cron.schedule('*/5 * * * *', async () => {
-    // run every 5 minutes
-    await BatchDownloadTpsC1(100);
-});
-// const main = async () => {
+// UNCOMMENT if you want to run the cron job in long running
+// cron.schedule('*/5 * * * *', async () => {
+//     // run every 5 minutes
 //     await BatchDownloadTpsC1(100);
-// }
-// main();
+// });
+
+// UNCOMMENT if you want to run the job once
+const main = async () => {
+    const count = 100; // number of TPS to download per batch finding incomplete
+    await BatchDownloadTpsC1(count);
+}
+main();
