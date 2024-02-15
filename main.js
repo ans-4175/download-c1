9603e2f7f2df9329dbc9e92121599c5c1099db3d
Anshorimuslim <ans4175@gmail.com>
2024-02-15 10:46:03 +0700

    Update cron schedule to run every 5 minutes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const BatchDownloadTpsC1 = require('./src/runners/BatchDownloadTpsC1')
 const cron = require('node-cron');
 
-cron.schedule('*/* * * * *', async () => {
+cron.schedule('*/5 * * * *', async () => {
     // run every 5 minutes
     await BatchDownloadTpsC1(100);
 });
