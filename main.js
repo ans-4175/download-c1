07cbe2dcb07a4be0ca1ba9cd40f0ad5d051776a4
Anshorimuslim <ans4175@gmail.com>
2024-02-16 09:05:28 +0700

    fix: main js passing parameters batchdownload

--- main.js
+++ main.js
@@ -29,10 +29,7 @@ const main = async () => {
   if (!iterative) {
     const count = 10000; // number of TPS to download per batch finding incomplete
     console.log("Begin performing download..");
-    await BatchDownloadTpsC1(count, {
-      provinsiCode: provinceCode,
-      kotaKabupatenCode: kotaKabupatenCode,
-    });
+    await BatchDownloadTpsC1(count, provinceCode, kotaKabupatenCode);
   } else {
     console.log("Begin performing download iteratively..");
     await IterativelyDownloadTpsC1(provinceCode, kotaKabupatenCode);
